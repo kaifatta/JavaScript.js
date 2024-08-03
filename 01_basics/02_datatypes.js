dbbec94b24be55501b7376a29592de0c7d2ffce3
Mohd Kaif <143071043+kaifatta@users.noreply.github.com>
2024-08-03 11:29:45 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3 + 3) 
+
+console.log("Kaif");
+
+
+let name = "Kaif"
+let age = 13
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolen => true/false
+// null => standlone value
+// undefined => 
+// symbol => unique
+
+
+// object 
+
+
+console.log(typeof undefined);// undefined
+console.log(typeof null); // object 
+
+
