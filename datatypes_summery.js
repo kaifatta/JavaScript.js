140ef7a717c809d4f63e37449c35960f9a4d0f7e
Mohd Kaif <143071043+kaifatta@users.noreply.github.com>
2024-08-08 09:49:31 +0000

    datatype summery

---
+++ datatypes_summery.js
@@ -0,0 +1,28 @@
+//  Primitive
+
+// 7 types : String, Number, Boolearn, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+const bigNumber = 34343434344n
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["superman", "naagraj", "doga"]
+let myObj = {
+    name: "kaif",
+    age: "19",
+}
+
